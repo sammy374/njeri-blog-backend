6937952f474ab36a9f2606195b47b280994d6908
sammy374 <sambloope@gmail.com>
2026-06-15 13:01:37 +0300

    Fix CORS for Vercel frontend

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://njeri-blog-m5n6-mauve.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
